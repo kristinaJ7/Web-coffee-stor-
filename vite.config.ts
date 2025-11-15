e07334eda1252dcae6c2ebd75950f70d2593e6d0
kristina filimonova <kristina.jk7@yandex.ru>
2025-11-15 09:34:19 +0300

    обновления проекта

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,12 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',          
+        info: 'info.html',         
+      },
+    },
+  },
 });
-
-
-
-
-
-
-
-
-
